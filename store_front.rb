ba0cf10d94b1932774b4715969b6fc81c6c2caf7
Fenote Berhane <72622074+fenotesb@users.noreply.github.com>
2021-02-26 21:57:00 -0500

    Update store_front.rb

--- store_front.rb
+++ store_front.rb
@@ -2,19 +2,25 @@
 puts "Hello whats your name:"
 name = gets.chomp
 
+#Use an array to represent the items in the store
 
 items = ["potato","milk", "egg","red onion","dried lemon","frankincense","moon dust","saffron","glass spice jar","red fabric","orange fabric","handicrafts","small persian rug","used medium space suit","heffalump shag rug","woozle hide purse"]
 
 puts "\nWelcome to my store #{name}\n\nHere is a list of items we have for you to purchase\n------------------------------------------------------------\n"
+
+#Print the list
 items.each do |item|
   print "*"
   puts item
 end
 
-
+#Create an array to hold user input
 cart = []
 error_cart= Array.new
 user_input = ""
+
+#use if and while loop to run program until the user enters key word 'done'
+
 while true  do
   puts "\nPlease enter what you would like to add to your cart (enter 'done' when finshed)"
   user_input = gets.chomp.downcase
@@ -26,6 +32,8 @@ while true  do
     cart.push(user_input)
   else
     puts "Sorry we dont have any #{user_input}. \nPlease enter something else and remeber to enter 'done' when your finished"
+    
+    #puts error items in its own array
     error_cart.push(user_input)
   end
 end
@@ -41,7 +49,7 @@ cart.each do |item|
   puts "#{item}"
 end
 
-
+# if user inputs an item not in the store the if statment will make sure it gets printed 
 if !error_cart.empty? 
   puts "\nBtw, we noticed you tried entering in some items that aren't part of the online Store. We don't have the following items yet but will let you know if they show up!:"
 
