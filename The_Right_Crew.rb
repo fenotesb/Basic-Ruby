fc74064388c94b76c03c4b34e9cf6cda35d287ad
Fenote Berhane <72622074+fenotesb@users.noreply.github.com>
2021-02-28 21:15:08 -0500

    Update The_Right_Crew.rb

--- The_Right_Crew.rb
+++ The_Right_Crew.rb
@@ -1,5 +1,5 @@
 Required_crew_size = 6
-
+#Used symbols and an array to represent the members of the crew
 avaiable_members = [
   :captain,
   :first_mate,
@@ -14,16 +14,16 @@ avaiable_members = [
   :combat_systems_officer,
 
 ]
-
+# Gets 6 random members
 random_members_chosen = avaiable_members.sample(Required_crew_size)
 
 puts "\****\Automated Flight Crew Advice:\****\"
-
+#print the random members chosen
 random_members_chosen.each do |chosen_one|
   puts "Use the #{chosen_one.to_s.gsub("\_", " ")}!"
 end
 puts " "
-
+# Used symbols and an array to warn that important personal might not be on the list
 [
   :captain,
   :navigator,
