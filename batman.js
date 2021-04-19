de7ebea169d40b825fc2c328c083ef56e0273f91
Fenote Berhane <72622074+fenotesb@users.noreply.github.com>
2021-04-18 23:10:45 -0400

    Add files via upload

---
+++ batman.js
@@ -0,0 +1,13 @@
+let batman = {
+  firstName: 'Bruce',
+  lastName: 'Wayne',
+  hometown: 'Gotham',
+  occupation: 'Batman',
+  catchPhrase: 'Where is it?!?',
+  name() {
+    return this.firstName + " " + this.lastName
+  }
+}
+
+console.log(batman)
+console.log(batman.name())
