9d467f635255082ffc64af93740ab371a588fca1
Fenote Berhane <72622074+fenotesb@users.noreply.github.com>
2021-04-22 22:00:01 -0400

    Add files via upload

---
+++ Shopping_Cart.js
@@ -0,0 +1,9 @@
+let eggs = { itemName: 'dozen eggs', quantity: 1 }
+let milk = { itemName: 'gallon of milk', quantity: 1 }
+let bread = { itemName: 'loaf of bread', quantity: 1 }
+
+let shoppingCart = [eggs, milk, bread]
+
+shoppingCart.forEach((item) => {
+  console.log(item.quantity + " " + item.itemName)
+})
