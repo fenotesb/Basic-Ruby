2f5a6eb0efe9fb3684979156dd5cd15fa03ca8ee
Fenote Berhane <72622074+fenotesb@users.noreply.github.com>
2021-05-03 20:19:24 -0400

    Update Shopping_Cart.js

--- Shopping_Cart.js
+++ Shopping_Cart.js
@@ -3,6 +3,8 @@ let eggs = { itemName: 'dozen eggs', quantity: 1 }
 let milk = { itemName: 'gallon of milk', quantity: 1 }
 let bread = { itemName: 'loaf of bread', quantity: 1 }
 let butter = {itemName: 'stick of butter', quantitiy: 1}
+let cheese = {itemName: 'lb of cheese', quantitiy: 1}
+
 
 let shoppingCart = [eggs, milk, bread, butter]
 
